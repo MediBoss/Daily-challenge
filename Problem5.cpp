86005d82f614ae21e5051ba06565e3a540443bf0
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-12 23:40:46 -0500

    Update Problem5.cpp

--- Problem5.cpp
+++ Problem5.cpp
@@ -3,6 +3,7 @@
 Coded by Medi W. Assumani
 Language : C++
 
+
                         Problem 5
 
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -11,3 +12,26 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 
 **/
+
+#include <iostream>
+
+using namespace std;
+
+int getSmallestNumber();
+int main(){
+
+
+
+  return 0;
+}
+
+int getSmallestNumber(int max){
+
+  int num = 1;
+
+  for(int i = 1; i < max; i++){
+    if(num % i == 0){
+      
+    }
+  }
+}
