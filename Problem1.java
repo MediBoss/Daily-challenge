11a6dde572148f37fb34c19582338c76e2d8d0c4
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-13 22:58:14 -0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,12 @@
+/**
+Coded By Medi W. Assumani
+Language : Java
+
+        DOCUMEMTATION
+        *************
+    This program finds the sum of all the multiples of
+
+    of 3 and 5 below 1000
+
+**/
 
