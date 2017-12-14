630e9992810de52bff16547ed8f75787c208cce2
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-14 11:38:36 -0500

    Update Probleme6.cpp

--- Probleme6.cpp
+++ Probleme6.cpp
@@ -1,1 +1,14 @@
+/**
 
+  Coded By : Medi Assumani
+
+
+  Language : C++
+
+
+  Problem Description :  Find the difference between the sum of the squares of the 
+
+  first one hundred natural numbers and the square of the sum.
+
+
+**/
