ba390a62dbc17d1b5c6e13c72ef6abc8f18fdf19
Medi W. Assumani <mediassumani49@gmail.com>
2018-03-12 10:43:15 -0400

    Create Problem6.java

---
+++ Problem6.java
@@ -0,0 +1,40 @@
+/*
+  Coded By : Medi W. Assumani
+  Language : JAVA
+  Problem: Determine if a number is a power of two.
+
+            Constraints
+            ***********
+
+1. Is the input number an int?
+Yes
+2. Can we assume the inputs are valid?
+No
+3. Is the output a boolean?
+Yes
+4. Can we assume this fits memory?
+Yes
+
+Test Cases
+None -> TypeError
+0 -> False
+1 -> True
+2 -> True
+15 -> False
+16 -> True
+*/
+import java.io.IOException;
+
+public class PowerOfTwo{
+  public static void main(String[] args){
+
+  }
+
+  /*
+    Function to check if a number is a power of two
+    @param : number : the number to be tested of type int
+  */
+  public static boolean isPowerOfTwo(int number){
+
+  }
+}
