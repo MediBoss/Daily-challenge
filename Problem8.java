708e3fadfe9db9061d92c1665e4576529ace2215
Medi Assumani <mediassumani49@gmail.com>
2018-03-16 01:11:14 -0400

    setting up problem 8

---
+++ Problem8.java
@@ -0,0 +1,16 @@
+/*
+
+Coded By : Medi W. Assumani
+Language : JAVA
+Problem: Write a function that rotates an array of size n by d elements
+*/
+
+public class Problem8{
+  public static void main(String[]args){
+
+  }
+
+  public static [int] rotate(int []array, int size, int elements){
+    
+  }
+}
