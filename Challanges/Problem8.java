0acad3aabe99e8efbe4faeac9910fbe3c37182d6
Medi Assumani <mediassumani49@gmail.com>
2018-03-16 18:43:14 -0400

    developing algorithm

--- Challanges/Problem8.java
+++ Challanges/Problem8.java
@@ -8,9 +8,41 @@ Problem: Write a function that rotates an array of size n by d elements
 public class Problem8{
   public static void main(String[]args){
 
+    int [] firstList = {1,2,3,4,5,6};
+
   }
 
-  public static [int] rotate(int []array, int size, int elements){
-    
+  /*
+  Function to return an array of elements shifted by d. From d to array.lenght
+
+  @param array : the list of integers
+  @param n : the length of the array
+  @param d: the number of elemntsto rotate by
+  */
+  public static int[] shiftRightElements(int []array, int n, int d){
+
+    int [] shiftedArray = new int[array.length - d];
+      for(int index = 0 ; index < shiftedArray.length; index++){
+        shiftedArray[index] = array[d];
+      }
+      return shiftedArray;
+  }
+
+
+  /*
+  Function to return an array of the unshifted elementsin the array
+
+  @param array : the list of integers
+  @param n : the length of the array
+  @param d: the number of elemntsto rotate by
+  */
+
+  public static int[] unshiftedElements(int []array, int n, int d){
+
+    int [] tempArray = new int[d];
+      for(int index = 0; index < tempArray.length; index++){
+        tempArray[index] = array[index];
+      }
+      return tempArray;
   }
 }
