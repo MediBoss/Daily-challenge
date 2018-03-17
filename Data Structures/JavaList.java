6e2357bfc68a0e3f93780d94d95f86e66c3bbb55
Medi Assumani <mediassumani49@gmail.com>
2018-03-16 22:59:29 -0400

    List

---
+++ Data Structures/JavaList.java
@@ -0,0 +1,63 @@
+/*
+  Coded By : Medi Assumani
+  Language : Java
+  Topic : List (resizable)
+
+  Purpose : Re-building Java's Arraylist with more functionalities and practicing algorithm skills
+*/
+
+public class JavaList{
+  // the default size of the list
+  private int size = 0;
+
+  //constructor for List Objec with initial size
+  //@ param size : the length of the List
+  public JavaList(int size){
+    this.size = size;
+  }
+
+  //constructor for list with no initial size
+  public JavaList(){
+    this.size = 0;
+  }
+
+  /*
+    Function to return the size of the List
+  */
+  public int getLength(){
+
+  }
+  // Function to append an element in the List
+  public void add(){
+
+  }
+  // function to remove an element in the list
+  public void delete(int num){
+
+  }
+  /*
+    function to set an elemnt in the list on a given position
+    @param : element :
+    @param : position:
+  */
+  public void set(int element, int position){
+
+  }
+  // function to get an elemnt from the list
+  public int get(){
+
+    return 0;
+  }
+    /*
+      Function to search if an elemnt is in the list
+      @param elemToBeSearched :
+    */
+  public bool search(int elemToBeSearched){
+
+    return 0;
+  }
+  // function to sort the List
+  public void sort(){
+
+  }
+}
