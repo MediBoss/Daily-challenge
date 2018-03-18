ad82839eb65366bf6f43bbb342be7ceb5c4ee0d7
Medi Assumani <mediassumani49@gmail.com>
2018-03-18 17:00:17 -0400

    implementing Linked lIst

--- Data Structures/Java/LinkedList.java
+++ Data Structures/Java/LinkedList.java
@@ -6,6 +6,14 @@
   Purpose : Implementing The Linked List Data Structure from scratch
 */
 
-public class LinkedList{
+public class LinkedList{//outer class
+  /*
+    * The Node class acts as an helper inner class for the Linked List Class
+    * Set as Static so we can access it without instanciating a Node Object
+    * @param AnytType :
+  */
+  private static class Node<AnyType>{
 
+
+  }
 }
