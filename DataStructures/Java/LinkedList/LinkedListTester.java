46d12f1d680810f630f6d9bdf0fd2f1029f2080b
Medi Assumani <mediassumani49@gmail.com>
2018-03-24 17:40:46 -0400

    testing

--- DataStructures/Java/LinkedList/LinkedListTester.java
+++ DataStructures/Java/LinkedList/LinkedListTester.java
@@ -3,10 +3,21 @@ public class LinkedListTester{
 
           // Testing the LinkedList methods
 
-      LinkedList<String> students = new LinkedList<String>();
+      LinkedList<String> classOneStudents = new LinkedList<String>();
+      classOneStudents.append("Medi Assumani");
+      classOneStudents.append("Ahmed Ahmed");
+      classOneStudents.append("Michal Shepard");
+      classOneStudents.append("Yves Songolo");
+      classOneStudents.append("Maceline Ambroise");
 
-      System.out.println(students.isEmpty());
-      students.print();
-      System.out.println(students.length());
+      System.out.println("Is the List Empty : " + classOneStudents.isEmpty());
+      System.out.println("The Length is : " + classOneStudents.length());
+      classOneStudents.print();
+      classOneStudents.delete("Yves Songolo");
+      classOneStudents.replace("Patrick Fortune","Medi Assumani");
+      System.out.println("The Length is now : " + classOneStudents.length());
+      classOneStudents.print();
+
+
   }
 }
