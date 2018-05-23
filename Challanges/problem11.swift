c1e52e48a83af4c2c1569d07977077d78d848771
Medi Assumani <mediassumani49@gmail.com>
2018-05-22 20:42:24 -0400

    Update problem11.swift

--- Challanges/problem11.swift
+++ Challanges/problem11.swift
@@ -1,1 +1,19 @@
+/**
+ 
+ Write a function that accepts a String as its only parameter, and returns true if the string reads the same when reversed, ignoring case.
 
+ * Coded By : Medi W. Assumani
+ * Language : Swift
+ * Program Name : Palindrome
+
+ */
+import Foundation
+
+func isPalindrome(string: String) -> Bool{
+    let lowerCasedInput = string.lowercased()
+    return lowerCasedInput.reversed().elementsEqual(lowerCasedInput)
+}
+
+print(isPalindrome(string: "rotator"))
+print(isPalindrome(string: "Hello, world"))
+print(isPalindrome(string: "Rats live on no evil star"))
