669044d6dc7970b871d6cc7abf4945bd67e10927
Medi Assumani <mediassumani49@gmail.com>
2018-05-23 01:39:53 -0400

    cleaning up spaces

--- Challanges/Problem1.java
+++ Challanges/Problem1.java
@@ -11,12 +11,9 @@ Language : Java
 **/
 
 public class MultiplesOfFiveAndThree{
-
   public static void main(String[] args){
-
+          
     System.out.println("\n\nThe sum of all Multiples of Five and Three is : "+ multiples(1000)+"\n\n");
-
-
   }
 /*
     *@param limit is the range or the highest number
@@ -24,17 +21,13 @@ public class MultiplesOfFiveAndThree{
     //function to get the sum of multiples of 3 and 5
     //postcondition : the sum is returned
     public static int multiples(int limit){
-            
+           
     int sum = 0;
     for(int k = 1; k < limit; k++){
-
         if(k % 3 == 0 || k % 5 == 0){
-
           sum+= k;
         }
-
       }
-
       return sum;
     }
 }
