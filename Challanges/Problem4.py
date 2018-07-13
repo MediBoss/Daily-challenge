d15cb5dd482eb54e21444d41f4f708e534e8261a
Medi Assumani <mediassumani49@gmail.com>
2018-07-12 22:15:29 -0400

    Update Problem4.py

--- Challanges/Problem4.py
+++ Challanges/Problem4.py
@@ -1,1 +1,22 @@
+"""
+uestion:
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+Example
+Suppose the following inputs are given to the program:
+3,5
+Then, the output of the program should be:
+[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]] 
+"""
+
+def compute(x,y):
+    for i in range(x):
+        for j in range(y):
+            
+def main():
+    first_number = input("Enter the first number: ")
+    second_number  = input("Enter the second number: ")
+
+if __name__ == "__main__":
+    main()
 
